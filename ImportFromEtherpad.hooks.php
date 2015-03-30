d141499ec4a84388053e9cf0ca36269184337f49
Christie Koehler <ck@christi3k.net>
2015-03-30 16:29:04 -0700

    First wave of doc and license cleanup.

--- ImportFromEtherpad.hooks.php
+++ ImportFromEtherpad.hooks.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Extension that adds ability to create wiki pages from etherpads.
+ * See https://github.com/christi3k/ImportFromEtherpad for more information.
+ * Copyright (C) 2015 Christie Koehler <ck@christi3k.net>
+ *
+ * @section LICENSE
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 class ImportFromEtherpadHooks {
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
--- ImportFromEtherpad.php
+++ ImportFromEtherpad.php
@@ -1,15 +1,101 @@
 <?php
+/**
+ * Extension that adds ability to create wiki pages from etherpads.
+ * See https://github.com/christi3k/ImportFromEtherpad for more information.
+ * Copyright (C) 2015 Christie Koehler <ck@christi3k.net>
+ *
+ * @section LICENSE
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @section Configuration
+ * The following settings may be overwritten in LocalSettings.php.
+ * Local configuration must be done after including this extension using
+ * require("extensions/ImportFromEtherpad.php");
+ * - $wgImportFromEtherpadSettings->pathToPandoc
+ * - $wgImportFromEtherpadSettings->pandocCmd
+ * - $wgImportFromEtherpadSettings->contentRegexes
+ * - $wgImportFromEtherpadSettings->hostRegexes
+ * - $wgImportFromEtherpadSettings->pathRegexes
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 // Must be run inside Mediawiki environment
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'ImportFromEtherpad_VERSION', '1.0' );
+define( 'ImportFromEtherpad_VERSION', '1.0.0' );
 
 class ImportFromEtherpadSettings {
+	/**
+	 * system path to pandoc executable
+	 * Non-Windows default: /usr/bin
+	 * Windows default: C:/Program Files/Pandoc/
+	 *
+	 * @var string $pathToPandoc
+	 */
 	public $pathToPandoc;
+
+	/** 
+	 * system command for pandoc
+	 * Non-Windows default: pandoc
+	 * Windows default: pandoc.exe
+	 *
+	 * @var string $pandocCmd
+	 */
 	public $pandocCmd;
+
+	/**
+	 * multi-dimensional array of regexes used to clean up converted content
+	 *
+	 * Should use the form array('find regex','replacement string'). Regex pattern
+	 * will automatically be placed inside delimiters, so indicate patterns without
+	 * preceding and trailing slashes.
+	 *
+	 * Example: 
+	 * $wgImportFromEtherpadSettings->contentRegexs[] = array("<br\s*\/>","\n")
+	 *
+	 * @var array $contentRegexes
+	 */
 	public $contentRegexs;
+
+	/**
+	 * multi-dimensional array of regexes used to determine suggested target title
+	 *
+	 * Should use the form array('find regex','replacement string'). Regex pattern
+	 * will automatically be placed inside delimiters, so indicate patterns without
+	 * preceding and trailing slashes.
+	 *
+	 * Example: 
+	 * $wgImportFromEtherpadSettings->hostRegexs[] = array('\w+\.\w+\.\w+','');
+	 *
+	 * @var array $contentRegexes
+	 */
 	public $hostRegexs;
+
+	/**
+	 * multi-dimensional array of regexes used to determine suggested target title
+	 *
+	 * Should use the form array('find regex','replacement string'). Regex pattern
+	 * will automatically be placed inside delimiters, so indicate patterns without
+	 * preceding and trailing slashes.
+	 *
+	 * Example: 
+	 * $wgImportFromEtherpadSettings->pathRegexs[] = array('\w+\.\w+\.\w+','');
+	 *
+	 * @var array $contentRegexes
+	 */
 	public $pathRegexs;
 }
 
@@ -19,7 +105,7 @@ $GLOBALS['wgImportFromEtherpadSettings'] = new ImportFromEtherpadSettings();
 //(borrowed from GraphViz extension)
 call_user_func( function() {
 
-	// Set execution path
+	// Set pandoc execution path
 	if ( stristr( PHP_OS, 'WIN' ) && !stristr( PHP_OS, 'Darwin' ) ) {
 		$GLOBALS['wgImportFromEtherpadSettings']->pathToPandoc = 'C:/Program Files/Pandoc/';
 		$GLOBALS['wgImportFromEtherpadSettings']->pandocCmd = 'pandoc.exe';
@@ -49,10 +135,10 @@ call_user_func( function() {
 		'path' => __FILE__,
 		'name' => 'Import From Etherpad',
 		'description' => 'Create a wiki page from an etherpad.',
-		'version' => '1.0',
+		'version' => '1.0.0',
 		'author' => '[http://christiekoehler.com Christie Koehler]',
 		'url' => 'https://github.com/christi3k/ImportFromEtherpad',
-		'license-name' => 'MPL 2.0'
+		'license-name' => 'GPL 3.0+'
 	);
 
 	$GLOBALS['wgAutoloadClasses']['SpecialImportFromEtherpad'] = $dir . '/SpecialImportFromEtherpad.php';
@@ -61,6 +147,7 @@ call_user_func( function() {
 	$GLOBALS['wgExtensionMessagesFiles']['ImportFromEtherpadAlias'] = $dir . '/ImportFromEtherpad.alias.php';
 	$GLOBALS['wgSpecialPages']['ImportFromEtherpad'] = 'SpecialImportFromEtherpad';
 
+	// register two javascript files that enable target title suggest
 	$GLOBALS['wgResourceModules']['ext.ImportFromEtherpad.main'] = array(
 		'scripts' => array(
 			'modules/ext.ImportFromEtherpad.main.js',
@@ -81,10 +168,11 @@ call_user_func( function() {
 		'remoteExtPath' => $dir,
 	);
 
+	// register hook that will load javascript modules defined above
 	$GLOBALS['wgHooks']['BeforePageDisplay'][] = 'ImportFromEtherpadHooks::onBeforePageDisplay';
+
+	// register hoook that will load the confg vars that javascript needs
 	$GLOBALS['wgHooks']['ResourceLoaderGetConfigVars'][] = 'ImportFromEtherpadHooks::onResourceLoaderGetConfigVars';
 } );
-
-
 
 /* vim:set ts=4 sw=4 sts=4 noexpandtab: */
